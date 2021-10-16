81a626aed13d15f32a366a07b086379965f265be
Troy Kollar <troykollar@gmail.com>
2021-10-16 19:11:26 -0400

    Deploy Next.js to gh-pages

--- _next/static/iaLYjVmoW3SCHbpDup8kp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-25dccc2e1a0724f8d892.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],"/about":["static/chunks/pages/about-b81994f66ea4a8d43593.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
