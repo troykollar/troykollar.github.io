ff6e2833a887e53755a4a9fde5aed34ad070b7d1
Troy Kollar <troykollar@gmail.com>
2021-10-17 11:22:35 -0400

    Add Footer to work

--- pages/work.tsx
+++ pages/work.tsx
@@ -1,10 +1,15 @@
+import Footer from "components/Footer/Footer";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <div className="professional">Professional</div>;
+  return (
+    <div className="professional">
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
