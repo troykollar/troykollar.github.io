db9c44d4dfe1377a082cda6595ae24ae376b7b86
Troy Kollar <troykollar@gmail.com>
2021-10-16 21:41:12 -0400

    Create Ima component with "typing" animation

---
+++ components/Ima.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import useInterval from "react-useinterval";
+import useTimeout from "usetimeout-react-hook";
+import { FunctionComponent, useState, useEffect } from "react";
+
+interface ImaProps {
+  text: string[];
+}
+
+const Ima: FunctionComponent<ImaProps> = ({ text }) => {
+  const [index, setIndex] = useState(0);
+  const [curText, setCurText] = useState(text[index]);
+  const [nextText, setNextText] = useState(text[index + 1]);
+
+  function increment() {
+    if (index === text.length - 1) setIndex(0);
+    else setIndex(index + 1);
+  }
+
+  const timerInterval = 5000;
+
+  useInterval(increment, timerInterval);
+
+  useEffect(() => {
+    let textToShow = text[index];
+    let shownText = "";
+    const typeSpeed = timerInterval / 3;
+    let timeouts = [];
+    textToShow.split("").forEach((letter, i) => {
+      timeouts.push(
+        setTimeout(() => {
+          shownText += letter;
+          setCurText(shownText);
+        }, i * 50),
+      );
+
+      timeouts.push(
+        setTimeout(() => {
+          shownText = shownText.slice(0, -1);
+          setCurText(shownText);
+        }, i * 50 + 2500),
+      );
+
+      return () => {
+        timeouts.forEach((timeout) => {
+          console.log(timeout);
+          clearTimeout(timeout);
+        });
+      };
+    });
+  }, [index]);
+
+  function createTypingCounters(shownText: string) {
+    shownText += "f";
+  }
+
+  useEffect(() => {
+    console.log("idk");
+  }, []);
+  return <h2>I am {curText}</h2>;
+};
+
+export default Ima;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,33 @@
+import Ima from "components/Ima";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home/Home.module.scss";
 
 const Home: NextPage = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className={styles.home}>
+      <Ima
+        text={[
+          "Troy Kollar.",
+          "a web developer.",
+          "a software engineer.",
+          "a quick learner.",
+          "a Node JS developer.",
+          "a leader.",
+          "adaptable.",
+          "A Vue JS developer.",
+          "eager to learn.",
+          "a friend.",
+          "a React deveoper.",
+          "kind of an idiot sometimes.",
+          "a Python developer.",
+          "a gamer.",
+          "a hobbyist.",
+        ]}
+      />
+    </div>
+  );
 };
 
 export default Home;
