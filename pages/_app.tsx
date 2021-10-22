dc5cbcb245a05933020f9e2488473cc3c06a6a3e
Troy Kollar <troykollar@gmail.com>
2021-10-21 20:38:52 -0400

    NavBubble works based on hover

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,23 +5,13 @@ import { RiMoonLine } from "react-icons/ri";
 import NavBubble from "../components/Nav/NavBubble";
 import Name from "../components/Nav/Name";
 import Footer from "components/Footer/Footer";
-import { MouseEventHandler, useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [bubbleActive, setBubbleActive] = useState(false);
-  function bubbleClick(event: any) {
-    event.stopPropagation();
-    setBubbleActive(!bubbleActive);
-  }
   return (
-    <div className="app dark" onClick={() => setBubbleActive(false)}>
+    <div className="app dark">
       <div className="nav-bar dark">
         <Name text="Troy Kollar" />
-        <NavBubble
-          active={bubbleActive}
-          closeBubble={() => setBubbleActive(false)}
-          click={bubbleClick}
-        />
+        <NavBubble />
         <div></div>
         {/* <RiMoonLine size={40} /> */}
       </div>
